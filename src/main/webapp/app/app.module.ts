539159baeafba44a59e5b365b0ff55f4677dfb03
Rohit Ergurala -X (rergural - CAPGEMINI TECHNOLOGY SERVICES INDIA LIMITED at Cisco) <rergural@cisco.com>
2017-05-14 23:54:59 +0530

    Initial Commit

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,57 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MybankingSharedModule, UserRouteAccessService } from './shared';
+import { MybankingHomeModule } from './home/home.module';
+import { MybankingAdminModule } from './admin/admin.module';
+import { MybankingAccountModule } from './account/account.module';
+import { MybankingEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        MybankingSharedModule,
+        MybankingHomeModule,
+        MybankingAdminModule,
+        MybankingAccountModule,
+        MybankingEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MybankingAppModule {}
